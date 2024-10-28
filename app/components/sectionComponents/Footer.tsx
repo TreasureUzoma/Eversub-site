f2f6181eaf0f6a2c0d83b9b85413f81b6d70b0d5
Treasure Uzoma <123127790+TreasureUzoma@users.noreply.github.com>
2024-10-28 13:04:50 +0100

    Update Footer.tsx

--- app/components/sectionComponents/Footer.tsx
+++ app/components/sectionComponents/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center py-[3rem]">
+    <footer className="mt-8 flex justify-center py-[3rem] md:mt-0">
       <div className="my_fixed_width">
         <h3 className="text-base font-bold font-grotesk">Eversub</h3>
         <div className="grid gap-3 place-items-center grid-cols-2 my-6">
