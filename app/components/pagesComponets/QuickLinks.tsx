3c6213a3aaa70f4fda1b6aae670e8268ed88117d
treasure <treasureuzoma650@gmail.com>
2024-10-30 05:52:50 -0700

    more ui tweaks

--- app/components/pagesComponets/QuickLinks.tsx
+++ app/components/pagesComponets/QuickLinks.tsx
@@ -4,12 +4,12 @@ const QuickLinks = ({ page='' }) => {
   return (
     <section>
       <h4 className="text-[1rem] my-5 font-semibold">Quick Links</h4>
-      <div className="border-l-4 border-myBlack flex flex-col pl-4 py-4 mb-9 text-[0.9rem] space-y-2 font-semibold">
+      <div className="border-l-2 border-myBlack flex flex-col pl-4 py-4 mb-9 text-[0.9rem] space-y-2">
         <Link href="/privacy">
           <span
             className={`${
               page === "privacy" ? "bg-myBlack text-white px-3" : ""
-            } inline-flex py-2 rounded-lg`}
+            } inline-flex py-2 rounded-lg text-[0.8rem] ml-2`}
           >
             Privacy Policy
           </span>
@@ -18,7 +18,7 @@ const QuickLinks = ({ page='' }) => {
           <span
             className={`${
               page === "terms" ? "bg-myBlack text-white px-3" : ""
-            } inline-flex py-2 rounded-lg`}
+            } inline-flex py-2 rounded-lg text-[0.8rem] ml-2`}
           >
             Terms & Condition
           </span>
