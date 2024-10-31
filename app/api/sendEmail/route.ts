05f4f6e7da48513cfb2802252f4bd6f140fd0a7e
Treasure Uzoma <123127790+TreasureUzoma@users.noreply.github.com>
2024-10-31 13:14:33 +0100

    Foundations for setting good error messages

--- app/api/sendEmail/route.ts
+++ app/api/sendEmail/route.ts
@@ -2,38 +2,33 @@ import { NextResponse } from 'next/server';
 import { db } from '@/libs/firebase';
 import { collection, addDoc, query, where, getDocs } from "firebase/firestore";
 
-// Email validation function
 const isValidEmail = (email: string): boolean => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 
 export async function POST(request: Request) {
   try {
     const { email } = await request.json();
 
-    // Check if email is provided and valid from backend too!
     if (!email || !isValidEmail(email)) {
       return NextResponse.json({ error: "A valid email is required" }, { status: 400 });
     }
 
-    // Check if the email already exists in the waitlist
     const emailQuery = query(collection(db, "waitlist"), where("email", "==", email));
     const existingEmails = await getDocs(emailQuery);
     
     if (!existingEmails.empty) {
-      return NextResponse.json({ error: "Email is already on the waitlist" }, { status: 400 });
+      return NextResponse.json({ error: "This email is already on our waitlist. Try a different one." }, { status: 409 });
     }
 
-    // Add the email to the waitlist
     await addDoc(collection(db, "waitlist"), { email, timestamp: new Date() });
 
     return NextResponse.json({
       message: "Email added to waitlist successfully!",
     });
   } catch (error) {
-    // Log the error for debugging
     console.error("Error adding email to waitlist:", error);
     
     return NextResponse.json(
-      { error: "Error adding email to waitlist" },
+      { error: "There was a problem adding your email. Please try again later." },
       { status: 500 }
     );
   }
