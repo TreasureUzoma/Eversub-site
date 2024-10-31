b41314bb142a8a5011a394347c39183fc9445b50
treasure <treasureuzoma650@gmail.com>
2024-10-31 04:01:49 -0700

    fixed deployment issue

--- app/api/sendEmail/route.ts
+++ app/api/sendEmail/route.ts
@@ -1,20 +1,25 @@
 // app/api/sendEmail/route.ts
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { db } from '@/libs/firebase';
 import { collection, addDoc } from "firebase/firestore";
 
 export async function POST(request: Request) {
   try {
     const { email } = await request.json();
-    
+
     if (!email) {
       return NextResponse.json({ error: "Email is required" }, { status: 400 });
     }
 
     await addDoc(collection(db, "waitlist"), { email, timestamp: new Date() });
 
-    return NextResponse.json({ message: "Email added to waitlist successfully!" });
+    return NextResponse.json({
+      message: "Email added to waitlist successfully!",
+    });
   } catch (error) {
-    return NextResponse.json({ error: "Error adding email to waitlist" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Error adding email to waitlist" },
+      { status: 500 }
+    );
   }
 }
